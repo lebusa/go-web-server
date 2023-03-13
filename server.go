db49be1566af5cf5e5025c4df2390dff2fa87238
Motebang Lebusa <lebusam@gmail.com>
2023-03-13 10:32:17 +0200

    create a counter on hits to count path

--- server.go
+++ server.go
@@ -5,10 +5,17 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
+
+var counter int
+var mutex = &sync.Mutex{}
 
 func main() {
 	http.HandleFunc("/", rootHandler)
+
+	http.HandleFunc("/count", counterHandler)
 
 	http.HandleFunc("/hi", greetingHandler)
 
@@ -23,4 +30,17 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 // handler to request directed to /lumela path
 func greetingHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi back at you!")
+}
+
+// echo string
+func echo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "greetings")
+}
+
+// increment counter
+func counterHandler(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock() // lock the counter
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+	mutex.Unlock() // realease the lock
 }
